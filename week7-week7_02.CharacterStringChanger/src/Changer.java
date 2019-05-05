0f768d195bd5ba4d442be631268d6018804d3419
swifferwarrior <kdhailey21@gmail.com>
2019-05-05 18:26:59 -0500

    Completed Exc. 2.

--- week7-week7_02.CharacterStringChanger/src/Changer.java
+++ week7-week7_02.CharacterStringChanger/src/Changer.java
@@ -17,13 +17,23 @@ public class Changer {
     public String change(String characterString) {
         //executes all added Changes for the character string in the order of 
         //their adding and returns the changed character string
-        String newCharacterString = "";
+        String newCharacterString = characterString;
 
         for (Change edit : changeList) {
-            newCharacterString = edit.change(characterString);
+            newCharacterString = edit.change(newCharacterString);
         }
 
         return newCharacterString;
     }
 
 }
+
+/* POST-SUBMIT NOTES */
+/*
+1. CHANGE() METHOD
+        Instead of initializing newCharacterString as a blank string, set it
+        to characterString so that it can be fed through the for() loop. Also,
+        for each occurence of the loop, it should change the very same
+        newCharacterString which is within the method. Changing characterString
+        changes a variable outside the method and will not persist.
+*/
