ff9b82ca2196aa807bd526394bac54aa149f00a0
swifferwarrior <kdhailey21@gmail.com>
2019-06-06 01:21:55 -0500

    Completed Exc. 10 [desktop]. Need more rationale for implementing interfaces.

--- week8-week8_10.NationalService/src/NationalService.java
+++ week8-week8_10.NationalService/src/NationalService.java
@@ -1,4 +1,4 @@
 public interface NationalService {
-    int getDaysLeft();
-    void work();
+    int getDaysLeft(); //returns number of days left on service
+    void work();        //reduces the working days by one. Working days number can't be negative.
 }
