f0919b76474e0850e1c4ea438db8dc00e0358fa6
root <root@Swiffer.localdomain>
2019-06-19 00:35:18 -0500

    Completed Exc. 12.1. Need to review HashMap vs Map

--- week8-week8_12.OnlineShop/src/Main.java
+++ week8-week8_12.OnlineShop/src/Main.java
@@ -2,6 +2,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        // write some test code here
+        Storehouse store = new Storehouse();
+        store.addProduct("milk", 3, 10);
+        store.addProduct("coffee", 5, 7);
+
+        System.out.println("prices:");
+        System.out.println("milk:  " + store.price("milk"));
+        System.out.println("coffee:  " + store.price("coffee"));
+        System.out.println("sugar: " + store.price("sugar"));
     }
 }
--- week8-week8_12.OnlineShop/src/Storehouse.java
+++ week8-week8_12.OnlineShop/src/Storehouse.java
@@ -1,21 +1,41 @@
-import java.util.Collections;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Set;
 
 public class Storehouse {
+    private Map<String, Integer> product;
+    private Map<String, Integer> productAmount;
 
+    public Storehouse() {
+        this.product = new HashMap<String, Integer> ();
+        this.productAmount = new HashMap<String, Integer> ();
+    }
+    
     public void addProduct (String product, int price, int stock){
         //adds a product whose price and number of stocks to the storehouse
+        for (int i = 0; i < stock; i++){
+            this.product.put(product, price);
+            this.productAmount.put(product, stock);
+        }
     }
     
     public int price (String product) {
         //returns the price of teh parameter product
         //if the produt is not available in teh storehouse, the method returns -99
-        return -99;
+        if(this.product.containsKey(product)){
+            return this.product.get(product);
+        } else {
+            return -99;
+        }
     }
     
-    public int stock (String product){
+    public int stock (String product) {
         //returns the stock of the parameter product
-        return 0;
+        if (this.productAmount.containsKey(product)){
+            return this.productAmount.get(product);
+        } else {
+            return 0;
+        }
     }
     
     public boolean take (String product){
@@ -23,6 +43,12 @@ public class Storehouse {
         //returns true if the object was available in the storehouse
         //if product not in storehouse, method returns false
         //product cannot go below zero
+        if (this.productAmount.containsKey(product)){
+            int newStock = this.productAmount.get(product);
+            if (this.productAmount.
+            return true;
+        } else 
+
         return false;
     }
     
@@ -31,3 +57,15 @@ public class Storehouse {
         return null;
     }
 }
+
+/* POST-SUBMIT NOTES */
+/*
+1. NEW OBJECTS
+    You can declare an abstract variable, but when instantiating it, it needs to be
+    specified. (I.e. Map is abstract, but HashMap is more specific)
+    
+    Quote: Inside the storehouse, the prices (and soon also the stocks) of the 
+    products have to be stored into a Map<String, Integer> variable! The type of 
+    the object so created can be HashMap, but you should use the interface Map 
+    for the variable type (see 40.4.2)
+*/
