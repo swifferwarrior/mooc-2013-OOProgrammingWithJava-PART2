0ca29cb0bbe30d98495fbe89cdaa37bd5e264b94
swifferwarrior <kdhailey21@gmail.com>
2019-06-21 11:32:20 -0500

    Compiler error on Exc 14. 'Student is not abstract...'

--- week8-week8_14.StudentsSortedByName/src/Student.java
+++ week8-week8_14.StudentsSortedByName/src/Student.java
@@ -1,4 +1,4 @@
-public class Student {
+public class Student implements Comparable <Student> {
 
     private String name;
 
@@ -14,5 +14,18 @@ public class Student {
     public String toString() {
         return name;
     }
+    
+    //@Override
+    public int comparedTo(Student other){
+        return this.name.compareToIgnoreCase(other.getName());
+    }
+    
 
 }
+
+/* POST-SUBMIT NOTES */
+/*
+1. COMPARED TO METHOD
+        String has it's own .compareTo() method, but regardless the method will
+        return a 1, 0 , or -1. This means it will still return an integer.
+*/
