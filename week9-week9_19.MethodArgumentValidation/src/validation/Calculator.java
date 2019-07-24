7a961d83534f6b53902d5e891e58fc8c501eb1dd
root <root@Swiffer.localdomain>
2019-07-23 23:32:36 -0500

    Completed Exc. 19.

--- week9-week9_19.MethodArgumentValidation/src/validation/Calculator.java
+++ week9-week9_19.MethodArgumentValidation/src/validation/Calculator.java
@@ -4,6 +4,9 @@ public class Calculator {
 
     public int multiplication(int fromInteger) {
 
+        if (fromInteger < 0){
+            throw new IllegalArgumentException("Number must be positive.");
+        }
         int multiplication = 1;
         for (int i = 1; i <= fromInteger; i++) {
             multiplication *= i;
@@ -13,6 +16,13 @@ public class Calculator {
     }
 
     public int binomialCoefficient(int setSize, int subsetSize) {
+        if (setSize < 0 || subsetSize < 0){
+            throw new IllegalArgumentException("Set and subset must be positive.");
+        }
+        
+        if (subsetSize > setSize){
+            throw new IllegalArgumentException("Set must be larger than subset.");
+        }
 
         int numerator = multiplication(setSize);
         int denominator = multiplication(subsetSize) * multiplication(setSize - subsetSize);
--- week9-week9_19.MethodArgumentValidation/src/validation/Person.java
+++ week9-week9_19.MethodArgumentValidation/src/validation/Person.java
@@ -6,16 +6,32 @@ public class Person {
     private int age;
 
     public Person(String name, int age) {
-
-        this.name = name;
+        if (name == null || name.length() > 40 || name.isEmpty() || age > 120 || age < 0) {
+            throw new IllegalArgumentException();
+        }
         this.age = age;
+        this.name = name;
     }
 
     public String getName() {
+
         return name;
     }
 
     public int getAge() {
+
         return age;
     }
 }
+/* POST-SUBMIT NOTES */
+ /*
+1. EMPTY NAME
+        Remember, when checking for empty string use name.isEmpty() rather than
+        name.equals("");
+2. DON'T USE 'THIS'
+        Problem, exception is not being thrown because if statements are looking for 
+        this.name and this.age within the constructor. Remove '.this'!
+3. NULL GOES FIRST
+        Null has to be checked for first. What if null is the second test? What
+        happens to a name length that is less than 40 but still null?
+ */
