466e3f7c3731eda713b396dd67f6b157d2f9d578
swifferwarrior <kdhailey21@gmail.com>
2019-08-24 16:01:05 -0500

    WIP. Works as intended. Troubleshooting cont'd. Deleted a duplicate 'main' class in the default package. Now accessing the correct class.

--- week9-week9_26.PhoneSearch/src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-
-    public static void main(String[] args) {
-        // Start your program here
-        // ATTENTION: In your program, you can create only one instance of class Scanner!
-    }
-}
