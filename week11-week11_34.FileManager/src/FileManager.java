22f952939c3dcdfbc23ca89f456ea54d603102e8
swifferwarrior <kdhailey21@gmail.com>
2019-09-21 23:36:05 -0500

    Completed Exc. 34

--- week11-week11_34.FileManager/src/FileManager.java
+++ week11-week11_34.FileManager/src/FileManager.java
@@ -10,12 +10,33 @@ import java.util.Scanner;
 public class FileManager {
 
     public List<String> read(String file) throws FileNotFoundException {
-        return null;
+        //returns the lines of the parameter file in ArrayList form
+        File txt = new File(file);
+        Scanner reader = new Scanner(txt);
+        List<String> fileLines = new ArrayList<String>();
+
+        while (reader.hasNextLine()) {
+            String line = reader.nextLine();
+            fileLines.add(line);
+        }
+        return fileLines;
     }
 
     public void save(String file, String text) throws IOException {
+        //writes the string of the second argument into the file of the first argument
+        FileWriter writer = new FileWriter(file);
+        writer.write(text);
+        writer.close();
     }
 
     public void save(String file, List<String> texts) throws IOException {
+        FileWriter writer = new FileWriter(file);
+        String txt = "";
+        
+        for (String line : texts) {
+            txt += line + "\n";
+        }
+        writer.write(txt);
+        writer.close();
     }
 }
--- week11-week11_34.FileManager/src/Main.java
+++ week11-week11_34.FileManager/src/Main.java
@@ -1,10 +1,30 @@
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         FileManager t = new FileManager();
+        ArrayList<String> eng = new ArrayList<String>();
+        eng.add("One");
+        eng.add("Two");
 
+        for (String line : t.read("src/testinput1.txt")) {
+            System.out.println(line);
+        }
+
+        /* WRITING A LINE
+        System.out.println("");
+        System.out.println("Writing a line");
+        t.save("src/testinput1.txt", "Hello, buddy :)");*/
+
+
+        // WRITING A LIST
+       System.out.println("");
+        System.out.println("Writing a list");
+        t.save("src/testinput1.txt", t.read("src/testinput2.txt"));
+        
         for (String line : t.read("src/testinput1.txt")) {
             System.out.println(line);
         }
