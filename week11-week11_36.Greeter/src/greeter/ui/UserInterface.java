2dbe2e279d3910d342f3e650bc09ab3afa7527f2
swifferwarrior <kdhailey21@gmail.com>
2019-09-23 00:36:17 -0500

    Completed Exc. 36

--- week11-week11_36.Greeter/src/greeter/ui/UserInterface.java
+++ week11-week11_36.Greeter/src/greeter/ui/UserInterface.java
@@ -7,14 +7,34 @@ import javax.swing.JLabel;
 import javax.swing.WindowConstants;
 
 public class UserInterface implements Runnable {
+    /*
+    *Implement a user interface which displays the text "Hi!"
+    *The width of JFrame has to be at least 400px, height 200px
+    *Title should be Swing On
+    *Should be created and become visible inside the method run()
+    *Text components are added to the user interface with the method createComponents()*/
 
     private JFrame frame;
 
+    public UserInterface() {
+    }
+
     @Override
     public void run() {
+        frame = new JFrame("Swing on"); //new window with title "Swing On"
+        frame.setPreferredSize(new Dimension(400, 200)); //Set width and height
+        
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        createComponents(frame.getContentPane());
+        
+        frame.pack(); //Sizes the frame appropriately and...
+        frame.setVisible(true); //...made visible
     }
 
     private void createComponents(Container container) {
+        JLabel text = new JLabel("Hi!");
+        container.add(text);
     }
 
     public JFrame getFrame() {
