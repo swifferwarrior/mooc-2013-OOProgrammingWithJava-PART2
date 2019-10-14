7bdbfc4b801462d9d06cc8eb2f51119ddffec548
swifferwarrior <kdhailey21@gmail.com>
2019-10-13 19:10:32 -0500

    Example I'm making for tutorial video

--- week11-week11_43.GameOfLife/src/game/Main.java
+++ week11-week11_43.GameOfLife/src/game/Main.java
@@ -1,10 +1,20 @@
 package game;
 
 import gameoflife.Simulator;
+import game.PersonalBoard;
 
 public class Main {
 
     public static void main(String[] args) {
-        // test your program here
+        PersonalBoard board = new PersonalBoard(7, 5);
+
+        board.turnToLiving(0, 1);
+        board.turnToLiving(1, 0);
+        board.turnToLiving(1, 2);
+        board.turnToLiving(2, 2);
+        board.turnToLiving(2, 1);
+
+        System.out.println("Neighbours alive (0,0): " + board.getNumberOfLivingNeighbours(0, 0));
+        System.out.println("Neighbours alive (1,1): " + board.getNumberOfLivingNeighbours(1, 1));
     }
 }
